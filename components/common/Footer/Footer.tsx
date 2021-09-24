1fd3023be13bd43bdce1f80031e185b78754c50a
MekkaLabs <labsmekka@gmail.com>
2021-09-24 18:31:07 -0300

    Update Footer.tsx

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -30,15 +30,7 @@ const Footer: FC<Props> = ({ className, pages }) => {
     <footer className={rootClassName}>
       <Container>
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 border-b border-accent-2 py-12 text-primary bg-primary transition-colors duration-150">
-          <div className="col-span-1 lg:col-span-2">
-            <Link href="/">
-              <a className="flex flex-initial items-center font-bold md:mr-24">
-                
-                  <Logo />
-                
-              </a>
-            </Link>
-          </div>
+          
           <div className="col-span-1 lg:col-span-8">
             <div className="grid md:grid-rows-4 md:grid-cols-3 md:grid-flow-col">
               {[...links, ...sitePages].map((page) => (
